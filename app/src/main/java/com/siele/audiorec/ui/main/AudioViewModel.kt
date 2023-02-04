b25c466fcae83564b861d7221b528e8bebf4ef9c
kimsiele <sielekim.emmanuel@gmail.com>
2023-02-04 23:39:33 +0300

    code refactor

--- app/src/main/java/com/siele/audiorec/ui/main/AudioViewModel.kt
+++ app/src/main/java/com/siele/audiorec/ui/main/AudioViewModel.kt
@@ -1,5 +1,10 @@
 package com.siele.audiorec.ui.main
 
+import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -10,8 +15,11 @@ import com.siele.audiorec.data.model.AudioRecording
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,6 +27,8 @@ class AudioViewModel @Inject constructor(private val audioRecordingsDao: AudioRe
 
     val audios = mutableStateOf(audioRecordingsDao.getAudioRecording())
     val elapsedTime = mutableStateOf(0)
+
+   val  isPLaying = mutableStateOf<Map<Int,Boolean>>(emptyMap())
 
     fun insertRecordings(audioRecording: AudioRecording){
         viewModelScope.launch {
@@ -37,6 +47,132 @@ class AudioViewModel @Inject constructor(private val audioRecordingsDao: AudioRe
             }
         }
     }
+
+
+
+    fun getTimerLabel(value: Int): String {
+        return "${timerPadding(value/60)}:${timerPadding(value % 60)}"
+    }
+
+    fun timerPadding(value: Int): String {
+        return if (value<10){
+            ("0$value")
+        }else{
+            ""+value
+        }
+    }
+
+
+    fun pauseRecording(
+        audioRecorder: MediaRecorder,
+        isPaused: MutableState<Boolean>,
+        isRecording: MutableState<Boolean>
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            audioRecorder.pause()
+        }
+        isPaused.value = true
+        isRecording.value = false
+    }
+
+    fun resumeRecording(
+        audioRecorder: MediaRecorder,
+        isPaused: MutableState<Boolean>,
+        isRecording: MutableState<Boolean>
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            audioRecorder.resume()
+        }
+        isPaused.value = false
+        isRecording.value = true
+    }
+
+    fun recordAudio(
+        audioRecorder: MutableState<MediaRecorder?>,
+        filePath: String,
+        isRecording: MutableState<Boolean>,
+        isPaused: MutableState<Boolean>,
+        context: Context,
+        startTime: MutableState<Long>
+    ) {
+        audioRecorder.value = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(context)
+        } else {
+            MediaRecorder()
+        }
+
+        try {
+            audioRecorder.value?.apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                setOutputFile(filePath)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                prepare()
+                start()
+                startTime.value = System.currentTimeMillis()
+                isRecording.value = true
+                isPaused.value = false
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun stopRecording(
+        audioRecorder: MediaRecorder,
+        isRecording: MutableState<Boolean>,
+        endTime: MutableState<Long>
+    ) {
+        if (isRecording.value){
+            audioRecorder.stop()
+            endTime.value = System.currentTimeMillis()
+            isRecording.value = false
+            audioRecorder.apply {
+                reset()
+                release()
+            }
+        }
+    }
+
+
+    fun pausePlay(
+        mediaPlayer: MutableState<MediaPlayer?>,
+        isPlaying: MutableState<Boolean>
+    ) {
+        mediaPlayer.value?.pause()
+        isPlaying.value = false
+
+    }
+
+    fun playRecording(
+        audioRecording: AudioRecording,
+        isPLaying: MutableState<Boolean>,
+        mediaPlayer: MutableState<MediaPlayer?>
+    ) {
+        mediaPlayer.value = MediaPlayer()
+        try {
+            mediaPlayer.value?.apply {
+                reset()
+                setDataSource(audioRecording.filePath)
+                prepare()
+                start()
+            }
+            isPLaying.value = true
+
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+    }
+
+    /*fun setIsPlaying (audioRecording: AudioRecording, audioRecordings: List<AudioRecording>){
+        audioRecordings.find { it==audioRecording }?.isPlaying = true
+        audios.value= flowOf(audioRecordings)
+    }
+
+    fun setIsPaused (audioRecording: AudioRecording, audioRecordings: List<AudioRecording>){
+        audioRecordings.find { it==audioRecording }?.isPlaying = false
+        audios.value = flowOf(audioRecordings)
+    }*/
 
 
 }
